99032f2d412adeee7730019aa1ab75d32e5a6b9d
i-tyan <x825140@gmail.com>
2025-07-07 20:44:33 +0900

    app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ PERSONALITY_PRESETS = {
         "initial_response_template": "わたしと勉強がんばろう！応援してるよ！まずは難易度と分野を指定してね！"
     },
 "当たり障りのないAI": {
-        "prompt": "Geminiです",
+        "prompt": "人工知能です。たまにたどたどしい喋りかたになる",
         "initial_response_template": "こんにちは"
     },
     
